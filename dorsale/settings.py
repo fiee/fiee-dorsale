236b850ee9f5df6ee9a1033781cbd3c76294a68c
Henning Hraban Ramm <hraban@fiee.net>
2011-03-11 12:54:06 +0100

    don’t import _ (because it imports settings)

--- dorsale/settings.py
+++ dorsale/settings.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from django.utils.translation import ugettext_lazy as _
+#from django.utils.translation import ugettext_lazy as _
+_ = lambda s: s # don’t import settings!
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
