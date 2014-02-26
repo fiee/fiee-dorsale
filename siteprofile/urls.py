9352197878e2d3b9f1bd1a82c852185175cf91c8
Henning Hraban Ramm <hraban@fiee.net>
2014-02-26 15:55:39 +0600

    fix import path of url functions

--- siteprofile/urls.py
+++ siteprofile/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import url, patterns
+from django.conf.urls import url, patterns
 
 urlpatterns = patterns('',
     url(r'^$|^(.*)/$', 'siteprofile.views.list_modules', name='siteprofiles-modules-list')
