5a95384b4dbde839764335116ade1f80de96c58c
Henning Hraban Ramm <hraban@fiee.net>
2014-06-03 12:42:47 +0600

    bump version to 0.0.7

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 import os
 
 setup(name='fiee-dorsale',
-      version='0.0.6',
+      version='0.0.7',
       description=u'Django base classes to handle management data like site, creation & change date & user',
       keywords='site group owner changed created mine',
       author='Henning Hraban Ramm',
@@ -31,6 +31,6 @@ setup(name='fiee-dorsale',
                    'Topic :: Utilities',
                    'Natural Language :: English',
                    'Natural Language :: German',],
-      install_requires=['Django>=1.6', 'django-registration',],
+      install_requires=['Django>=1.6', 'django-registration', 'django-async-messages'],
       zip_safe=False,
       )
