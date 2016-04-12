00c197b0e26dc3276a6a73cfcfd915aae0a5fba8
Henning Hraban Ramm <hraban@fiee.net>
2016-04-12 15:51:13 +0600

    bump version (first really usable version)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 import os
 
 setup(name='fiee-dorsale',
-      version='0.0.10',
+      version='0.1.0',
       description=u'Django base classes to handle management data like site, creation & change date & user',
       keywords='site group owner changed created mine',
       author='Henning Hraban Ramm',
@@ -20,7 +20,7 @@ setup(name='fiee-dorsale',
       include_package_data = True,
       package_data = {'': ['*.rst', 'locale/*/LC_MESSAGES/*.*', 'templates/*/*.*', 'templates/*/*/*.*', ]},
       # see http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=['Development Status :: 3 - Alpha',
+      classifiers=['Development Status :: 2 - Beta',
                    'Environment :: Web Environment',
                    'Framework :: Django',
                    'Intended Audience :: Developers',
