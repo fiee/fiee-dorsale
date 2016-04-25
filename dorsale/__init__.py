ce639b66fd920b84f7165376531cc87ff1cac671
Henning Hraban Ramm <hraban@fiee.net>
2016-04-25 17:08:29 +0600

    moved validators to fiee-edition, since they weren’t used anywhere else

--- dorsale/__init__.py
+++ dorsale/__init__.py
@@ -5,4 +5,4 @@ from django.utils.translation import ugettext_lazy as _
 verbose_name = _(u'fiëé dorsale')
 verbose_name_plural = _(u'fiëé dorsale')
 
-__all__ = ['admin', 'context_processors', 'export', 'forms', 'json', 'managers', 'middleware', 'models', 'settings', 'tools', 'urls', 'validators', 'views', 'widgets']
+__all__ = ['admin', 'context_processors', 'export', 'forms', 'json', 'managers', 'middleware', 'models', 'settings', 'tools', 'urls', 'views', 'widgets']
