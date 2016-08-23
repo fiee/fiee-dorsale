f1df00f3c7d880a92687bc07330dd2bdc802aee1
Henning Hraban Ramm <hraban@fiee.net>
2016-08-23 16:27:09 +0200

    fix: ordering might be a list

--- dorsale/templatetags/order_tags.py
+++ dorsale/templatetags/order_tags.py
@@ -5,6 +5,11 @@ register = template.Library()
 @register.simple_tag
 def orderby(fieldname, request_ordering):
     """check if the fieldname is in request_ordering and then revert it"""
+    try:
+        # might be a list
+        request_ordering = ','.join(request_ordering)
+    except:
+        pass
     if fieldname in request_ordering:
         minus = '-'+fieldname
         if minus in request_ordering:
