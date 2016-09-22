7da1ac418f6105acd9998386b2de1010332252ef
Henning Hraban Ramm <hraban@fiee.net>
2016-09-22 13:35:42 +0200

    fix: ordering parameter is sometimes a list, previous handling was not enough

--- dorsale/templatetags/order_tags.py
+++ dorsale/templatetags/order_tags.py
@@ -1,21 +1,25 @@
+from __future__ import unicode_literals
 from django import template
-
+import logging
+logger = logging.getLogger(__name__)
 register = template.Library()
 
 @register.simple_tag
 def orderby(fieldname, request_ordering):
     """check if the fieldname is in request_ordering and then revert it"""
-    try:
-        # might be a list
-        request_ordering = ','.join(request_ordering)
-    except:
-        pass
-    if fieldname in request_ordering:
-        minus = '-'+fieldname
-        if minus in request_ordering:
-            ordering = request_ordering.replace(minus, fieldname)
+    request_ordering = list(request_ordering)
+    ordering = []
+    for ro_field in request_ordering:
+        if fieldname in ro_field:
+            minus = '-'+fieldname
+            try:
+                if minus in ro_field:
+                    ordering.append(ro_field.replace(minus, fieldname))
+                else:
+                    ordering.append(ro_field.replace(fieldname, '-'+fieldname))
+            except AttributeError as ex:
+                logger.error('field name "%s" should be a string, but is a %s' % (ro_field, type(ro_field)))
+                logger.exception(ex)
         else:
-            ordering = request_ordering.replace(fieldname, '-'+fieldname)
-    else:
-        ordering = fieldname
-    return ordering
+            ordering.append(fieldname)
+    return ','.join(ordering)
